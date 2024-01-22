b1eb030b7944bcd99401945d66f366c1fab6e3b3
Your name <omowunmidaud1@gmail.com>
2024-01-22 22:57:05 +0100

    updated

---
+++ src/components/Content.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import './content.css';
+import shopify from '../../src/images/Shopify.com svg.png'
+import coinbase from '../../src/images/Coinbase svg.png'
+import dropbox from '../../src/images/Dropbox svg.png'
+import intercom from '../../src/images/Intercom svg.png'
+import marvel from '../../src/images/Marvel svg.png'
+import automatic from '../../src/images/Automattic svg.png'
+import video from '../../src/images/zap.png'
+import audio from '../../src/images/zap-1.png'
+import calender from '../../src/images/zap-2.png'
+import security from '../../src/images/zap-3.png'
+import image from '../../src/images/Rectangle 1.png'
+
+const companies = [
+
+  {
+    icon: shopify,
+  },
+  {
+    icon: coinbase,
+  },
+  {
+    icon: dropbox,
+  },
+  {
+    icon: intercom,
+  },
+  {
+    icon: marvel,
+  },
+  {
+    icon: automatic,
+  },
+
+];
+
+const Content = () => {
+  return (
+
+    <div className='content'>
+
+        <section className='content-top'>
+
+            <p>Join 1,500+ companies already video conferencing the ClearLink way</p>
+
+            <div>
+                { 
+                    companies.map((object)=>{
+                      return <img src={object.icon} alt='' />
+                    })
+                }
+            </div>
+
+        </section>
+
+        <section className='content-bottom'>
+
+            <div className='bottom-up'>
+                <p style={{color: 'blue', fontWeight: '600'}}>The ClearLink Advantage</p>
+                <h1 style={{fontSize: '40px', marginTop: '-10px'}}>Why choose ClearLink?</h1>
+                <p style={{fontSize: '18px', marginTop: '-1em'}}>In a world where connection is everything, ClearLink takes the lead. Our <br /> cutting-edge video conferencing app offers:</p>
+            </div>
+
+            <div className='bottom-down' style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '3em'}}>
+
+                <section style={{display: 'flex', flexDirection: 'column', width: '60%'}}>
+
+                    <div className='offers'>
+
+                        <div className='offer'>
+
+                            <img src={video} alt='' />
+                            <h3>Crystal-clear HD video</h3>
+                            <p>No more pixelation or blurriness – just <br /> stunning, lifelike clarity that brings your <br /> team closer in meetings.</p>
+
+                        </div>
+
+                        <div className='offer'>
+
+                            <img src={audio} alt='' />
+                            <h3>Noise-canceling audio</h3>
+                            <p>Say goodbye to distractions with our <br /> advanced audio tech for crisp, <br /> interruption-free conversations.</p>
+
+                        </div>
+
+                    </div>
+
+                    <div className='offers'>  
+
+                        <div className='offer'>
+
+                            <img src={calender} alt='' />
+                            <h3>Scheduling made easy</h3>
+                            <p>Streamline your agenda with ClearLink's <br /> intuitive scheduling. Set up meetings, <br /> send invitations, and receive reminders in <br /> one place.</p>
+
+                        </div>
+
+                        <div className='offer'>
+
+                            <img src={security} alt='' />
+                            <h3>Bank-grade security</h3>
+                            <p>Your privacy is our priority with bank- <br /> grade security protocols safeguarding <br /> your meetings and data from unwanted <br /> intruders.</p>
+
+                        </div>
+
+                    </div>
+
+                </section>
+
+                <img src={image} alt="" style={{width: '35%', marginTop: '2em'}} />
+
+            </div>
+
+        </section>
+
+    </div>
+  )
+}
+
+export {Content}
---
+++ src/components/Header.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import logo from '../../src/images/Frame 18.png'
+import avatar from '../../src/images/Avatar group.png'
+import stars from '../../src/images/Stars.svg'
+import image from '../../src/images/Frame 25.png'
+import image1 from '../../src/images/Frame 10.png'
+
+const Header = () => {
+  return (
+
+    <header className="App-header">
+
+        <nav>
+
+          <img src={logo} alt="" />
+
+          <div className="navbar">
+
+            <a href="http">Products</a>
+            <a href="http">Solutions</a>
+            <a href="http">Resources</a>
+            <a href="http">Pricing</a>
+
+          </div>
+
+          <div className="actions">
+
+            <button id='btn1'>Talk to sales</button>
+            <button id='btn2'>Sign up for free</button>
+
+          </div>
+
+        </nav>
+
+        <section className='hero'>
+
+          <aside>
+
+            <h1>Uniting the world, <br /> one video call at a time</h1>
+            <p>Experience the future of communication with ClearLink – <br /> where crystal-clear video conferencing meets <br /> unparalleled simplicity.</p>    
+
+            <div className="header-actions">
+
+              <button id='btn3'>Start your free trial</button>
+              <button id='btn4'> <img src={image1} alt="" /> Discover AI assistant</button>
+
+            </div>
+
+            <div className='reviews'>
+
+              <img src={avatar} alt="" />
+
+              <div>
+                <span style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '80%'}}> <img src={stars} alt="" /> 5.0</span>
+                <p style={{marginTop: '5px'}}>from 3,000+ reviews</p>
+              </div>
+
+            </div>
+
+          </aside>
+
+          <img src={image} alt="" style={{width: '45%', marginTop: '2em'}} />
+
+        </section>
+
+    </header>
+
+  )
+}
+
+export {Header}
